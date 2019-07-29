b9d16de08c85fbacac3b1aa4f432f677b0e05bae
Thomas Fisher-York <tly2@cornell.edu>
2019-07-29 10:46:54 -0400

    Old simulation of several generations.

--- generations.pl
+++ generations.pl
@@ -6,6 +6,7 @@ use List::Util qw(min max sum);
 use Getopt::Long;
 use Math::GSL::RNG  qw( :all );
 use GenotypeSim qw ( :all );
+use Genotype;
 
 # the idea here is to start with a set of unrelated genotypes
 # and let them pair up and produce offspring,
@@ -22,6 +23,9 @@ use GenotypeSim qw ( :all );
    my $pop = 20;         # population size
    my $turnover = 5;     # this many births and deaths each generation
    my $mafspec = 'delta,0.25';  # minor allele frequency
+   my $type = 3;
+   my $n_gens_out = 4;
+my $sample_size = undef;
  
    GetOptions(
               'nsnps=i' => \$n_snps, # 
@@ -29,6 +33,9 @@ use GenotypeSim qw ( :all );
               'population=i' => \$pop,
               'turnover=i' => \$turnover, 
               'maf|minor_allele_frequence=s' => \$mafspec, # 0 < $maf <= 0.5
+             'type=i' => \$type,
+              'nout=i' => \$n_gens_out,
+              'sample_size=i' => \$sample_size,
              );
 
    my $mafs = GenotypeSim::draw_mafs($the_rng, $mafspec, $n_snps);
@@ -36,51 +43,70 @@ use GenotypeSim qw ( :all );
    # generate a data set of genotypes, by choosing an initial population,
    # and generation several generations ... 
    my @genotypes = ();
-   my @dataset_genotypes = ();
-
-   for (1..$pop) {      # generate the initial population of genotypes
-      push @genotypes, GenotypeSim::draw_genotype_from_population($the_rng, $mafs, $n_snps);
+   my @stored_genotypes = ();
+   my %id_genotypeobj = ();
+   my $id = undef;
+   for $id = (1..$pop) {      # generate the initial population of genotypes
+        my $gobj = Genotype->new_from_population($the_rng, $mafs, $_);
+         push @genotypes, $gobj;
+      $id_genotypeobj{$id} = 
    }
 
-   if(0){
-   for (1..$n_generations) {
-      my ($i, $j) = ( gsl_rng_uniform_int($the_rng->raw(), $pop),  gsl_rng_uniform_int($the_rng->raw(), $pop) );
-      my $offspring = GenotypeSim::draw_offspring_genotype($the_rng, $genotypes[$i], $genotypes[$j] );
-      my $i_die =  gsl_rng_uniform_int($the_rng->raw(), $n_snps); # this one 'dies', i.e. gets replaced by the new offspring genotype.
-      $genotypes[$i_die] = $offspring;
-      my $i_data = gsl_rng_uniform_int($the_rng->raw(), $pop); # this one gets 'observed' i.e. stored in the data set which will then be analyzed
-      push @dataset_genotypes, $genotypes[$i_data];
-   }
-
-} else {
-   for (1..$n_generations) {
-      my ($i, $j, $k) = ( gsl_rng_uniform_int($the_rng->raw(), scalar @genotypes),  
-                          gsl_rng_uniform_int($the_rng->raw(), scalar @genotypes), 
-                          gsl_rng_uniform_int($the_rng->raw(), scalar @genotypes), 
-                        );
-      
-      push @genotypes, GenotypeSim::draw_offspring_genotype($the_rng, $genotypes[$i], $genotypes[$j] ); # 1st offspring
-      if (1 or gsl_rng_uniform($the_rng->raw()) < 0.75){
-         push @genotypes, GenotypeSim::draw_offspring_genotype($the_rng, $genotypes[$i], $genotypes[$j] ); # if do this, it is FS of 1st offspring
+   if ($type == 1) { # one offspring, replacing one of old genotypes, per generation
+      for (1..$n_generations) {
+         my ($i, $j) = ( gsl_rng_uniform_int($the_rng->raw(), $pop),  gsl_rng_uniform_int($the_rng->raw(), $pop) );
+         my $offspring = GenotypeSim::draw_offspring_genotype($the_rng, $genotypes[$i], $genotypes[$j] );
+         my $i_die =  gsl_rng_uniform_int($the_rng->raw(), $n_snps); # this one 'dies', i.e. gets replaced by the new offspring genotype.
+         $genotypes[$i_die] = $offspring;
+         my $i_data = gsl_rng_uniform_int($the_rng->raw(), $pop); # this one gets 'observed' i.e. stored in the data set which will then be analyzed
+         push @stored_genotypes, $genotypes[$i_data];
       }
-          push @genotypes, GenotypeSim::draw_offspring_genotype($the_rng, $genotypes[$i], $genotypes[$k] );
-          #   my $i_die =  gsl_rng_uniform_int($the_rng->raw(), $n_snps); # this one 'dies', i.e. gets replaced by the new offspring genotype.
-          #   $genotypes[$i_die] = $offspring;
-          #   my $i_data = gsl_rng_uniform_int($the_rng->raw(), $pop); # this one gets 'observed' i.e. stored in the data set which will then be analyzed
-          #      push @dataset_genotypes, $genotypes[$i_data];
-       }
-        @dataset_genotypes = @genotypes;
+
+   } elsif($type == 2) {
+      for (1..$n_generations) {
+         my ($i, $j, $k) = ( gsl_rng_uniform_int($the_rng->raw(), scalar @genotypes),
+                             gsl_rng_uniform_int($the_rng->raw(), scalar @genotypes),
+                             gsl_rng_uniform_int($the_rng->raw(), scalar @genotypes),
+                           );
+      
+         push @genotypes, GenotypeSim::draw_offspring_genotype($the_rng, $genotypes[$i], $genotypes[$j] ); # 1st offspring
+         if (1 or gsl_rng_uniform($the_rng->raw()) < 0.75) {
+            push @genotypes, GenotypeSim::draw_offspring_genotype($the_rng, $genotypes[$i], $genotypes[$j] ); # if do this, it is FS of 1st offspring
+         }
+         push @genotypes, GenotypeSim::draw_offspring_genotype($the_rng, $genotypes[$i], $genotypes[$k] );
+         #   my $i_die =  gsl_rng_uniform_int($the_rng->raw(), $n_snps); # this one 'dies', i.e. gets replaced by the new offspring genotype.
+         #   $genotypes[$i_die] = $offspring;
+         #   my $i_data = gsl_rng_uniform_int($the_rng->raw(), $pop); # this one gets 'observed' i.e. stored in the data set which will then be analyzed
+         #      push @dataset_genotypes, $genotypes[$i_data];
+      }
+      @stored_genotypes = @genotypes;
+   } elsif ($type == 3) { # in each gen, choose $pop pairs u.a.r., one offspring from each
+      for my $i_gen (1..$n_generations) {
+         my @new_genotypes = ();
+         for my $i_mating (1..$pop) {
+            my ($i, $j) = ( gsl_rng_uniform_int($the_rng->raw(), scalar @genotypes),
+                            gsl_rng_uniform_int($the_rng->raw(), scalar @genotypes)
+                          );
+
+            push @new_genotypes, GenotypeSim::draw_offspring_genotype($the_rng, $genotypes[$i], $genotypes[$j] ); # 1st offspring
+         }
+         push @stored_genotypes, @new_genotypes if($i_gen > $n_generations - $n_gens_out);
+         @genotypes = @new_genotypes;
+      }
    }
 
-   print STDERR "n samples: ", scalar @dataset_genotypes, "\n";
+   print STDERR "n samples: ", scalar @stored_genotypes, "\n";
 
-   while (my ($i, $g1) = each @dataset_genotypes) {
-      for (my $j = $i+1; $j < scalar @dataset_genotypes; $j++) {
+   my @dataset_genotypes = $the_rng->shuffle(@stored_genotypes);
+   
+my @sample_genotypes =  (defined $sample_size)? @dataset_genotypes[0..$sample_size-1] : @dataset_genotypes;
+   while (my ($i, $g1) = each @sample_genotypes) {
+      for (my $j = $i+1; $j < scalar @sample_genotypes; $j++) {
          #   print STDERR "$i $j \n";
-         my $g2 = $dataset_genotypes[$j];
+         my $g2 = $sample_genotypes[$j];
          #    GenotypeSim::print_genotypes_in_columns($g1, $g2);
          my ($an, $ad, $hn, $hd, $gpc_count) = GenotypeSim::agmr_hgmr($g1, $g2);
-         print "XXX   $an  $ad   $hn  $hd  ", $an/$ad, "  ", ($hd > 0)? $hn/$hd : '---',  "  ", GenotypeSim::paircode_count_string($gpc_count), "\n";
+         print "$i$j   $an  $ad   $hn  $hd  ", $an/$ad, "  ", ($hd > 0)? $hn/$hd : '---',  "  ", GenotypeSim::paircode_count_string($gpc_count), "\n";
       }
    }
 }
