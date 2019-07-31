99d04f6c7bd24e3833837dd4e4e378e60c9ef23e
Thomas Fisher-York <tly2@cornell.edu>
2019-07-31 13:27:54 -0400

    Just generate generations of genotypes and output as fasta. genotype pairwise comparison moved to genotype_pair_analysis.pl

--- ggg.pl
+++ ggg.pl
@@ -8,95 +8,131 @@ use Math::GSL::RNG  qw( :all );
 use GenotypeSim qw ( :all );
 use Genotype;
 
+# ggg.pl : "generate generations of genotypes"
+
 # the idea here is to start with a set of unrelated genotypes
-# and let them pair up and produce offspring,
-# 
+# and let them pair up randomly and produce offspring,
+# for some specified number of generations.
 
 {                               # main
-   my $the_rng = Math::GSL::RNG->new();
+
 
    # defaults:
    my $n_snps = 10;             # number of snps in each sample.
-   #   my $relationship = 'PO'; # PO: parent-offspring. Or FS (full sib), HS (half sib), etc. ...
-   #   my $n_reps = 100; # number or replicates (i.e. simulated sample pairs, each with $n_snps in both samples
-   my $n_generations = 100;
-   my $pop = 20;                # population size
-   #   my $turnover = 5;     # this many births and deaths each generation
-   my $mafspec = 'delta,0.25';  # minor allele frequency
-   my $type = 3;
-   my $n_gens_out = 4;
+   my $n_generations = 3;
+   my $pop = 50;                # population size
+   my $mafspec = 'delta,0.25';  # minor allele frequency 
+   my $max_pedigree_depth = 2;
+   my $rng_type = $gsl_rng_mt19937;
+   my $rng_seed = undef;
+   my $n_gens_out = $n_generations;
    my $sample_size = undef;
+   my $fasta_character_set = '012'; # anything else gives aA, aa, AA.
+   my $fasta_filename = undef;
 
    GetOptions(
               'nsnps=i' => \$n_snps, # 
               'ngens|ngenerations=i' => \$n_generations,
               'population=i' => \$pop,
-              #           'turnover=i' => \$turnover, 
               'maf|minor_allele_frequence=s' => \$mafspec, # 0 < $maf <= 0.5
-              'type=i' => \$type,
+              'depth_limit=i' => \$max_pedigree_depth,
+              'rng_type=s' => \$rng_type, # mt -> 'Mersenne Twister'
+              'seed=i' => \$rng_seed,
               'nout=i' => \$n_gens_out,
               'sample_size=i' => \$sample_size,
+              'character_set=s' => \$fasta_character_set,
+              'output_fasta_filename=s' => \$fasta_filename,
              );
 
+   # ############## set up the random number generator ##########################################
+   if ($rng_type eq 'sys') {
+      $rng_type = $gsl_rng_default;
+   } elsif ($rng_type eq 'mt') {
+      $rng_type = $gsl_rng_mt19937;
+   } elsif ($rng_type eq 'lux') {
+      $rng_type = $gsl_rng_ranlxd2;
+   }
+   print STDERR "RNG type: $rng_type \n";
+   my $the_rng = (defined $rng_seed)? Math::GSL::RNG->new($rng_type, $rng_seed) : Math::GSL::RNG->new($rng_type) ; # RNG
+   # ############################################################################################
+
+   if(!defined $fasta_filename){
    my $mfspc = $mafspec;
    $mfspc =~ s/,/-/g;
    my $ssbit = "_samplesize" . ((defined $sample_size)? $sample_size : $n_gens_out * $pop);
-   my $output_filename =
+   $fasta_filename =
      "pop" . $pop . "_nsnps" . $n_snps .
        "_ngens" . $n_generations . "-" . $n_gens_out .
-        "_maf-" . $mfspc . $ssbit;
-   print STDERR "output file name: ", $output_filename, "\n";
-# exit;
+         "_maf-" . $mfspc . $ssbit;
+}
+   print STDERR "output file name: ", $fasta_filename, "\n";
+
+
+   # ##################################################################################################
+   # ########### get the initial generation of Genotype objects with the given set of mafs ############
 
    my $mafs = GenotypeSim::draw_mafs($the_rng, $mafspec, $n_snps);
 
+   # my $g0 = Genotype->new_from_population($the_rng, $mafs, 0, 666);
+   # my $str012 = $g0->genotype_012string();
+   # print "$str012 \n", $g0->genotype_string(), "\n";
+   # my $g1 = Genotype->new_from_012string($the_rng, $str012, 0, 777);
+   # my $str012_1 = $g1->genotype_012string();
+   # print "$str012_1 \n", $g1->genotype_string(), "\n";
+   # exit;
+
+
    # generate a data set of genotypes, by choosing an initial population,
    # and generation several generations ... 
-   my @this_generation = (); # a set of Genotype objects from 1 generation.
+   my @initial_generation = (); # a set of Genotype objects from 1 generation.
    my @stored_generations = ();
    my %id_genotypeobj = ();
-   my ($generation, $id) = (0, undef);
-   for $id (1..$pop) {  # generate the initial population of genotypes
+   my ($generation, $id) = (0, 0);
+   for (1..$pop) {      # generate the initial population of genotypes
       my $gobj = Genotype->new_from_population($the_rng, $mafs, $generation, $id);
-      push @this_generation, $gobj;
+      #   print "X gen, id:  ", $gobj->get_generation(), "  ", $gobj->get_id(), "\n";
+      push @initial_generation, $gobj;
       $id_genotypeobj{$id} = $gobj;
+      $id++;
    }
-   push @stored_generations, \@this_generation; # if($n_gens_out >= $n_generations);
+ 
+   push @stored_generations, \@initial_generation; # if($n_gens_out >= $n_generations);
 
+   # ###################################################################################################
+
+
+   # ###################################################################################################
+   # ########### get specified number of generations, each one produced by random mating of ######
+   # ########### individuals in the preceding generation.  #########
    # in each gen, choose $pop pairs u.a.r., one offspring from each
+
+   my @this_generation = @initial_generation;
    for my $i_gen (1..$n_generations-1) {
       my @next_generation = ();
-      for my $i_mating (1..$pop) {
-         $id++;
+      for (1..$pop) {
+
          my ($i, $j) = ( gsl_rng_uniform_int($the_rng->raw(), scalar @this_generation),
                          gsl_rng_uniform_int($the_rng->raw(), scalar @this_generation)
                        );
 
-         # print $i, " ", ref $this_generation[$i], "\n";
-     #    my ($an, $ad, $hn, $hd, $gpc_count) = GenotypeSim::agmr_hgmr($this_generation[$i], $this_generation[$j]);
-         #  print "$i$j   $an  $ad   $hn  $hd  ", $an/$ad, "  ", ($hd > 0)? $hn/$hd : '---',  "  ", GenotypeSim::paircode_count_string($gpc_count), "\n";
-
          my $gobj = Genotype->new_offspring($the_rng, $this_generation[$i], $this_generation[$j], $i_gen, $id ); # 1st offspring
          $id_genotypeobj{$id} = $gobj;
          push @next_generation, $gobj;
+         $id++;
       }
-      push @stored_generations, \@next_generation; # if($i_gen >= $n_generations - $n_gens_out);
+      push @stored_generations, \@next_generation; 
       @this_generation = @next_generation;
    }
 
-   for my $a_gen (@stored_generations){
-      print genotypes_as_fasta($a_gen);
-      # for my $gobj (@$a_gen){
-      #    print $gobj->
-      # }
-   }
+   # ############### done generating genotype generations ################################################
+
 
    my @kept_generations = @stored_generations[-$n_gens_out..-1]; # discard all but last $n_gens_out generations
    my @dataset_genotype_objects = ();
-   for my $gobjs (@kept_generations){
+   for my $gobjs (@kept_generations) {
       push @dataset_genotype_objects, @$gobjs;
    }
-    @dataset_genotype_objects = $the_rng->shuffle(@dataset_genotype_objects);
+   @dataset_genotype_objects = $the_rng->shuffle(@dataset_genotype_objects);
    my @sample_genotype_objects;
    if (defined $sample_size  and $sample_size < scalar @dataset_genotype_objects) {
       print STDERR "Showing results for ", $sample_size, " samples out of  ", scalar @dataset_genotype_objects, "\n";
@@ -105,28 +141,38 @@ use Genotype;
       @sample_genotype_objects = @dataset_genotype_objects;
       print STDERR "Showing results for all  ", scalar @dataset_genotype_objects, " samples. \n";
    }
-   open my $fhout, ">", $output_filename;
-   while (my ($i, $g1) = each @sample_genotype_objects) {
-      print STDERR "$i out of ", scalar @sample_genotype_objects, "\n";
-      for (my $j = $i+1; $j < scalar @sample_genotype_objects; $j++) {
-         #   print STDERR "$i $j \n";
-         my $g2 = $sample_genotype_objects[$j];
-         #    GenotypeSim::print_genotypes_in_columns($g1, $g2);
-         my ($an, $ad, $hn, $hd, $gpc_count) = GenotypeSim::agmr_hgmr($g1, $g2);
-         printf $fhout "%3i %3i %4i %4i %4i %4i  %5.4f %5.4f   ", ($i, $j, $an, $ad, $hn, $hd, $an/$ad, ($hd > 0)? $hn/$hd : -100);
-         printf $fhout "%s \n", GenotypeSim::paircode_count_string($gpc_count);
-      }
-   }
+
+   @dataset_genotype_objects = sort { $a->get_id() <=> $b->get_id() } @dataset_genotype_objects;
+
+  # ###################################################################################################
+  # ###############  output  ######
+  # fasta
+   open my $fh_out, ">", $fasta_filename . '.fasta';
+ #  for my $a_gen (@dataset_genotype_objects) {
+      print $fh_out genotypes_as_fasta(\@dataset_genotype_objects, $fasta_character_set);
+#   }
+   close $fh_out;
+
 }
 
 sub genotypes_as_fasta{
-   my $set_of_genotypes = shift;
+   my $genotype_objects = shift; # array ref
+   my $character_set = shift // '012'; 
+   my $separator = shift // '';
    my $fasta_string = '';
 
-   for my $gobj (@$set_of_genotypes) {
-      my ($id, $gtstring, $gen, $parents) = ( $gobj->get_id(), $gobj->genotype_string(), $gobj->get_generation(), $gobj->get_parents() );
-      $fasta_string .= ">$id   $gen $parents \n";
-      $fasta_string .= "$gtstring \n";
+   for my $gobj (@$genotype_objects) {
+      my ($gen, $id, $pedigree) = ($gobj->get_generation(), 
+                                   $gobj->get_id(),
+                                   #  $gobj->get_parents(), 
+                                   $gobj->get_pedigree()
+                                  );
+      my $gstring =  ($character_set eq '012')?
+        $gobj->genotype_012string($separator) :
+          $gobj->genotype_aAstring($separator);
+      $fasta_string .= ">$id   $gen   $pedigree \n";
+      $fasta_string .= "$gstring \n";
    }
    return $fasta_string;
 }
+
