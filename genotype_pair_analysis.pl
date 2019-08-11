9402cd562bf7fc49a24b5cbffae06bbb18c8c67e
Thomas Fisher-York <tly2@cornell.edu>
2019-08-11 13:05:44 -0400

    known_pairs_only option. Tell it to just compare genotypes 0 and 1, 2 and 3, etc.

--- genotype_pair_analysis.pl
+++ genotype_pair_analysis.pl
@@ -17,11 +17,13 @@ use Genotype;
    my $sample_size = undef;
    my $rng_type = $gsl_rng_mt19937;
    my $rng_seed = undef;
+   my $known_pairs_only = 0;
 
    GetOptions(
               'sample_size|size=i' => \$sample_size, # 
               'rng_type=s' => \$rng_type,
               'seed=s' => \$rng_seed,
+              'known_pairs_only!' => \$known_pairs_only,
              );
 
    # ############## set up the random number generator ##########################################
@@ -49,22 +51,20 @@ use Genotype;
       }
    }
 
-   # ###########   get subset of size $sample_size   ################################
-   if (defined $sample_size  and  ($sample_size < scalar @genotype_objects)) {
-      @genotype_objects = $the_rng->shuffle(@genotype_objects);
-      @genotype_objects = @genotype_objects[0..$sample_size-1];
+   # ###########   get subset of size $sample_size pairs  ################################
+   if (defined $sample_size  and  (2*$sample_size < scalar @genotype_objects)) {
+   #   @genotype_objects = $the_rng->shuffle(@genotype_objects);
+      @genotype_objects = @genotype_objects[0..2*$sample_size-1];
    }
 
    # ########################    compare pairs   ####################################
 
 #   my $output_filename;
 #   open my $fhout, ">", $output_filename;
-   while (my ($i, $g1) = each @genotype_objects) {
-      print STDERR "$i out of ", scalar @genotype_objects, "\n";
-      for (my $j = $i; $j < scalar @genotype_objects; $j++) {
-         #   print STDERR "$i $j \n";
-         my $g2 = $genotype_objects[$j];
-         #    GenotypeSim::print_genotypes_in_columns($g1, $g2);
+
+   if($known_pairs_only){ # just compare 0 with 1, 2 with 3, etc.
+      for (my $i=0; $i < scalar @genotype_objects; $i += 2) {
+         my ($g1, $g2) = @genotype_objects[$i,$i+1];
          my ($an, $ad, $hn, $hd, $gpc_count) = GenotypeSim::agmr_hgmr($g1, $g2);
          printf  "%3i %3i %4i %4i %4i %4i  %5.4f %5.4f   ", 
            ($g1->get_id(), $g2->get_id(), $an, $ad, $hn, $hd, $an/$ad, ($hd > 0)? $hn/$hd : -100);
@@ -72,6 +72,24 @@ use Genotype;
          
          printf  "%s   ", GenotypeSim::paircode_count_string($gpc_count);
          printf  "%s  %s \n", $g1->get_pedigree(), $g2->get_pedigree;
+
+      }
+   } else {                     # all vs all
+      while (my ($i, $g1) = each @genotype_objects) {
+         print STDERR "$i out of ", scalar @genotype_objects, "\n";
+     
+         for (my $j = $i; $j < scalar @genotype_objects; $j++) {
+            #   print STDERR "$i $j \n";
+            my $g2 = $genotype_objects[$j];
+            #    GenotypeSim::print_genotypes_in_columns($g1, $g2);
+            my ($an, $ad, $hn, $hd, $gpc_count) = GenotypeSim::agmr_hgmr($g1, $g2);
+            printf  "%3i %3i %4i %4i %4i %4i  %5.4f %5.4f   ", 
+              ($g1->get_id(), $g2->get_id(), $an, $ad, $hn, $hd, $an/$ad, ($hd > 0)? $hn/$hd : -100);
+ 
+         
+            printf  "%s   ", GenotypeSim::paircode_count_string($gpc_count);
+            printf  "%s  %s \n", $g1->get_pedigree(), $g2->get_pedigree;
+         }
       }
    }
 
